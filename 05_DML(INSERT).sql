dc890b025c3041e0c754742a2e480d98b64579cc
eunsik jung <bamtol2100@naver.com>
2024-05-01 13:06:48 +0900

    Oracle 공부

--- 05_DML(INSERT).sql
+++ 05_DML(INSERT).sql
@@ -12,3 +12,23 @@ INSERT INTO EMPLOYEES e VALUES(EMPLOYEES_SEQ.nextval,
 								0.5,
 								NULL,
 								60);
+								
+							
+							
+							 
+-- EMPLOYEE_ID, LAST_NAME, EMAIL, HIRE_DATE, JOB_ID 만 넣기
+INSERT INTO EMPLOYEES (EMPLOYEE_ID,
+						 LAST_NAME,
+						 EMAIL,
+						 HIRE_DATE,
+						 JOB_ID)
+			VALUES(EMPLOYEES_SEQ.nextval,
+					'은식',
+					'test@test.com',
+					sysdate,
+					'IT_PROG');
+
+
+
+
+ 							
