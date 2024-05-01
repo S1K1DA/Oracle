099338a7122cd901b7bfa3bea578173f4f8e551c
eunsik jung <bamtol2100@naver.com>
2024-05-01 12:57:10 +0900

    Oracle 공부

---
+++ 05_DML(INSERT).sql
@@ -0,0 +1,14 @@
+SELECT * FROM EMPLOYEES e ;
+
+
+INSERT INTO EMPLOYEES e VALUES(EMPLOYEES_SEQ.nextval,
+								'길동',
+								'홍',
+								'TEST',
+								'010-5096-2429',
+								sysdate,
+								'IT_PROG',
+								10000,
+								0.5,
+								NULL,
+								60);
